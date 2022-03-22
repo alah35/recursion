1a29a1879d710b8cfa8b8106952b03dfa27b01d3
alah35 <maxnet663@gmail.com>
2022-03-22 14:56:37 +0300

    Task 4 Done

--- Tasks/Task4.cpp
+++ Tasks/Task4.cpp
@@ -1,17 +1,14 @@
 
 void get_amount_evnum(long long num, int &ans) {
-    ans = 0;
 
-    if (num / 10 > 0) {
+    if (num / 10 > 0)
         get_amount_evnum(num / 10, ans);
-    }
 
-    if ((num % 10) % 2 == 0)
-        ans++;
+     ans += ((num % 10) % 2) == 0;
 }
 
 void task4() {
-    int ans;
+    int ans = 0;
     long long n;
     std::cout << "Enter a very long number:" << std::endl;
     std::cin >> n;
